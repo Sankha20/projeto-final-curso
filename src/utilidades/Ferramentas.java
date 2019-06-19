a41af1179e299d99cd7153906be23d656775e4c0
Victor Pontes <=>
2019-06-19 16:11:26 -0300

    Comentários para documentação utilidades.Ferramentas

--- src/utilidades/Ferramentas.java
+++ src/utilidades/Ferramentas.java
@@ -2,9 +2,18 @@ package utilidades;
 
 import javax.swing.JOptionPane;
 
-
+/**
+ * Classe com métodos estáticos de utilidade geral
+ * @author Victor Pontes
+ */
 public class Ferramentas {
     
+    /**
+     * Abre caixa de diálogo pedindo confirmação.
+     * @param msg A mensagem a ser exibida
+     * @return <code>boolean</code>
+     * Se foi confirmado ou não
+     */
     public static boolean confirma(String msg) {
         int response;
         response = JOptionPane.showConfirmDialog(null, msg, "Confirmar", JOptionPane.YES_NO_OPTION);
@@ -12,12 +21,20 @@ public class Ferramentas {
         return (response == 0);
     }
 
+    /**
+     * Abre caixa de alerta
+     * @param msg A mensagem a ser exibida
+     */
     public static void alerta(String msg) {
         JOptionPane.showMessageDialog(null, msg, "Alerta!", JOptionPane.INFORMATION_MESSAGE);
     }
 
+    /**
+     * Abre caixa de erro
+     * @param msg A mensagem a ser exibida
+     */
     public static void erro(String msg) {
-        JOptionPane.showMessageDialog(null, msg, "Ops...", JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(null, msg, "Erro!", JOptionPane.ERROR_MESSAGE);
     }
-
+    
 }
